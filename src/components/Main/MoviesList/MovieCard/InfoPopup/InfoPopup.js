7b5b8f910a6c1fcbdd9e47060eacfdc55a277b8d
Vladyslav Aleksandrov <vladyslav.aleksandrov.s@gmail.com>
2021-03-12 18:20:17 +0200

    Components-2: close movie card popup on outside and button click

--- src/components/Main/MoviesList/MovieCard/InfoPopup/InfoPopup.js
+++ src/components/Main/MoviesList/MovieCard/InfoPopup/InfoPopup.js
@@ -4,15 +4,34 @@ import PropTypes from 'prop-types';
 import './InfoPopup.scss';
 
 const InfoPopup = (props) => {
+  const handleOutsideClick = event => {
+    if (!event.target.closest('.info-popup') && props.isInfoPopupOpen) {
+      props.toggleInfoPopup();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  });
+
+  const handleButtonClick = callback => {
+    props.toggleInfoPopup();
+    callback();
+  }
+
   return ( 
   <div className={`movie-card__info-popup info-popup ${props.isInfoPopupOpen ? 'visible' : 'hidden'}`}>
     <div className="info-popup__button-close" onClick = {props.toggleInfoPopup}>
       x
     </div>
-    <div className="info-popup__button info-popup__button-edit" onClick={props.toggleEditModal}>
+    <div className="info-popup__button info-popup__button-edit" onClick={() => handleButtonClick(props.toggleEditModal)}>
       Edit
     </div>
-    <div className="info-popup__button info-popup__button-delete" onClick={props.toggleDeleteModal}>
+    <div className="info-popup__button info-popup__button-delete" onClick={() => handleButtonClick(props.toggleDeleteModal)}>
       Delete
     </div>
   </div> );
